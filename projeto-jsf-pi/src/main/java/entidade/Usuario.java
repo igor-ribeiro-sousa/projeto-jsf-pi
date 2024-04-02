35e9aa3c8bb9894f5bc6b5805de0d341393e3c42
Igor Ribeiro <antonio.sousa@klab.com.br>
2024-04-02 11:59:51 -0300

    Finalização

--- projeto-jsf-pi/src/main/java/entidade/Usuario.java
+++ projeto-jsf-pi/src/main/java/entidade/Usuario.java
@@ -9,7 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "TBL_USR")
